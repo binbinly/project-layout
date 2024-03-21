8dc577c5e4e73920f068778495b69da4103a9e4c
gboy <admin@gmail.com>
2024-03-21 16:48:08 +0800

    edit

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"project-layout/cmd"
+)
 
+// @title swagger 接口文档
+// @version 2.0
+// @description
+
+// @contact.name binbinly
+// @contact.url https://github.com/binbinly
+// @contact.email binbinly@hotmail.com
+
+// @license.name MIT
+// @license.url
+
+// @securityDefinitions.apikey  Token
+// @in                          header
+// @name                        Token
+
+// @host http://127.0.0.1:9100
+// @BasePath /v1
+
+// @externalDocs.description  OpenAPI
+// @externalDocs.url          https://swagger.io/resources/open-api/
+func main() {
+	cmd.Execute()
 }
